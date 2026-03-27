1c783957a32123185de19ee17e5622eab040da2a
Martin Gawron <martin.gawron@proton.me>
2026-03-27 19:00:14 +0100

    Fix: set correct baseURL and build workflow for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/static");
   return {
+    pathPrefix: "/0cms-example-eleventy/",
     dir: {
       input: "src",
       output: "_site"
